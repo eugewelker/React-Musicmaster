82fc0a7b4713baa77ade9f21e54c97263be15a76
eugewelker <eugewelker88@gmail.com>
2019-10-14 00:53:12 +0100

    Styling with Bootstrap

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,17 @@
 import React,{Component} from 'react'
+import './app.css';
+import {Button,FormControl,Form} from 'react-bootstrap';
 
 class App extends Component {
   render(){
     return(
-      <div>
-        <div className="App-title">Music Master from App</div>
-          <div>
-          <input placeholder="search an artist..."/>
-          <button>Button</button>
+      <div className="App">
+        <div className="App-title">Music Master</div>
+          <div class="row justify-content-center">
+          <Form inline>
+          <FormControl placeholder="search an artist..."/>
+          <Button>Submit</Button>
+          </Form>
           </div>
           <div className="Profile">
             <div>Artist Picture</div>
